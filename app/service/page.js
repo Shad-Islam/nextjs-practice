9bb696d1c4113f694f36dbacbd186f1ae02e8bdf
Shad-islam <m.shadislam01@gmail.com>
2025-06-16 02:37:15 +0600

    Created Service listing page at /service

--- app/service/page.js
+++ app/service/page.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function ServicePage() {
-  return <div className="text-center mt-10">This is Service Page</div>;
+  return (
+    <div className="text-center mt-10">
+      <p>This is Service Page</p>
+      <ul>
+        <li>
+          <Link href="/service/1">Service 1</Link>
+        </li>
+        <li>
+          <Link href="/service/2">Service 2</Link>
+        </li>
+        <li>
+          <Link href="/service/3">Service 3</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
